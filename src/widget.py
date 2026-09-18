85da9cc32e1242702c9f2a964157f274dfcadff3
Герасимова Ника <gerasimovanika09@gmail.com>
2026-09-19 00:20:17 +0300

    Добавлена обработка даты

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from masks import get_mask_card_number, get_mask_account
+
+from datetime import datetime
+
+def mask_account_card(card_or_account: str) -> str:
+    parts = card_or_account.split()
+    number = parts[-1]
+
+    if parts[0] == "Счет":
+        masked_number = get_mask_account(number)
+        return parts[0] + " " + masked_number
+    else:
+        masked_number = get_mask_card_number(number)
+        return " ".join(parts[:-1]) + " " + masked_number
+
+def get_date(date: str) -> str:
+    date_object = datetime.fromisoformat(date)
+    return date_object.strftime("%d.%m.%Y")
